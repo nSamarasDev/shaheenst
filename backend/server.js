3ad6d6a979292daf9dfc160e59e0e64333eeeab2
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-28 11:01:20 -0400

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Shaheen backend API' });
@@ -10,5 +20,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
