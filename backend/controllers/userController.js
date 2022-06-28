003064b832b66f152014195b79aac2ed937461da
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-28 18:15:00 -0400

    userController.js updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,6 +69,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @dec    Get current user
+// @route  /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 // Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -79,4 +91,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
