6be324a6e44ccefde6ab4367beb6390cb4c0a359
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-29 11:49:59 -0400

    home.jsx update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
 
 function Home() {
   return (
-    <h1>
-        Home
-    </h1>
+    <>
+        <section className="heading">
+          <h1>What do you need to do</h1>
+          <p>Please choose from an option below</p>
+        </section>
+
+        <Link to='/new-employee' className='btn btn-reverse btn-block'>
+          <FaQuestionCircle /> Create employee information
+        </Link>
+
+        <Link to='/employees' className='btn btn-block'>
+          <FaTicketAlt /> View all employees
+        </Link>
+    </>
   )
 }
 
