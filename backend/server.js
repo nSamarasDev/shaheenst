7cb16149c69bd06ee408af7ca05e04071af518b3
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-30 13:36:00 -0400

    server.js updated for employee routes

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/employees', require('./routes/employeeRoutes'));
 
 app.use(errorHandler);
 
