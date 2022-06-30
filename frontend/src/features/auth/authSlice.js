7a7f76de2e25312af795eb2cb96e8c8e37193a7b
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-30 10:11:45 -0400

    authSlice.js updated to logout

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -36,6 +36,11 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user);
 });
 
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
