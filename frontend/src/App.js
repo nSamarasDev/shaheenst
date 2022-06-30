5d276ed56612d4365eb7471896cae3b7714a0a4a
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-30 13:40:12 -0400

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
+import NewEmployee from './pages/NewEmpolyee';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -17,6 +19,10 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+
+            <Route path='/new-employee' element={<PrivateRoute />}>
+              <Route path='/new-employee' element={<NewEmployee />} />
+            </Route>
           </Routes>
         </div>
       </Router>
