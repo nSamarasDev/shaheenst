e0894a6a6cff19a523574a62bd63aa38c877dff3
Nicholas Samaras <n.samaras4@outlook.com>
2022-07-06 14:13:57 -0400

    new function in redux added

--- frontend/src/features/employees/employeeSlice.js
+++ frontend/src/features/employees/employeeSlice.js
@@ -30,6 +30,26 @@ export const createEmployee = createAsyncThunk(
   }
 );
 
+// Get employees
+export const getEmployees = createAsyncThunk(
+  'employees/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await employeeService.getEmployees(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const employeeSlice = createSlice({
   name: 'employees',
   initialState,
@@ -46,6 +66,19 @@ export const employeeSlice = createSlice({
         state.isSuccess = true;
       })
       .addCase(createEmployee.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getEmployees.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getEmployees.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tickets = action.payload;
+      })
+      .addCase(getEmployees.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
