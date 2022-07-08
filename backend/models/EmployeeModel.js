348a173c9b83353cfc3dc404c3de36b27945e747
Nicholas Samaras <n.samaras4@outlook.com>
2022-07-08 15:34:15 -0400

    Lost geocoding because crash on reRender

--- backend/models/EmployeeModel.js
+++ backend/models/EmployeeModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const geocoder = require('../utils/geocoder');
 
 const EmployeeSchema = mongoose.Schema(
   {
@@ -23,30 +22,14 @@ const EmployeeSchema = mongoose.Schema(
     status: {
       type: String,
       required: true,
-      enum: ['new', 'prior employment', 'currently employeed'],
+      enum: ['new', 'closed', 'currently employeed'],
       default: 'new',
     },
     address: {
       type: String,
       required: [true, 'Please add an address'],
     },
-    location: {
-      // GeoJSON Point
-      type: {
-        type: String,
-        enum: ['Point'],
-      },
-      coordinates: {
-        type: [Number],
-        index: '2dsphere',
-      },
-      formattedAddress: String,
-      street: String,
-      city: String,
-      state: String,
-      zipcode: String,
-      country: String,
-    },
+
     email: {
       type: String,
       required: [true, 'Please add your email'],
@@ -82,25 +65,5 @@ const EmployeeSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-
-// Geocode & create location field
-EmployeeSchema.pre('save', async function (next) {
-  const loc = await geocoder.geocode(this.address);
-  this.location = {
-    trye: 'Point',
-    coordinates: [loc[0].longitude, loc[0].latitude],
-    formattedAddress: loc[0].formattedAddress,
-    street: loc[0].streetName,
-    city: loc[0].city,
-    state: loc[0].stateCode,
-    zipcode: loc[0].zipcode,
-    country: loc[0].countryCode,
-  };
-
-  // Do not save address in DB
-  this.address = undefined;
-
-  next();
-});
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
