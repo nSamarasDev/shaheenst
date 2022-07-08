894922bf859a8f448067b190fec9a598b1808cc8
Nicholas Samaras <n.samaras4@outlook.com>
2022-07-08 15:30:18 -0400

    EmployeeItem.jsx added

---
+++ frontend/src/components/EmployeeItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function EmployeeItem({employee}) {
+  return (
+    <div className='ticket'>
+        <div>{new Date(employee.createdAt).toLocaleString('en-US')}</div>
+        <div>{employee.firstName}</div>
+        <div className={`status status-${employee.status}`}>
+            {employee.status}
+        </div>
+        <Link to={`/employee/${employee._id}`} className='btn btn-reverse btn-sm'>
+            View
+        </Link>
+    </div>
+  )
+}
+
+export default EmployeeItem
