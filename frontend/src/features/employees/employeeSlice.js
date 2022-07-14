3730f162ea32a1cef1bce59f263d02695b48de94
Nicholas Samaras <n.samaras4@outlook.com>
2022-07-13 23:38:04 -0400

    employeeSlice.js updated

--- frontend/src/features/employees/employeeSlice.js
+++ frontend/src/features/employees/employeeSlice.js
@@ -90,6 +90,26 @@ export const closeEmployee = createAsyncThunk(
   }
 );
 
+// Update employee
+export const updateEmployee = createAsyncThunk(
+  'employees/update',
+  async (employeeId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await employeeService.updateEmployee(employeeId, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const employeeSlice = createSlice({
   name: 'employees',
   initialState,
